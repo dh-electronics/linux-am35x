c9b7e47db9bd32629b54a420d01fae827158b2cc
Ludwig Zenz <lzenz@dh-electronics.de>
2014-08-27 17:44:00 +0200

    add medeacc baseboard support

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -181,6 +181,7 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 
 		/* DHCM3517 and DHAM35XMEDEACC using UART1 */
 		DEBUG_LL_OMAP3(1, dhcm3517);
+		DEBUG_LL_OMAP3(1, dham35xmedeacc);
 
 	} while (0);
 }
